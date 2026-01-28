9ab0c0dddc6b0f30c8b536e00be23fa79b503c31
fab <fabiopalma2504@gmail.com>
2026-01-28 23:04:06 +0100

    first commit

---
+++ src/main/java/com/bitvault/pass/BitvaultApplication.java
@@ -0,0 +1,13 @@
+package com.bitvault.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitvaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitvaultApplication.class, args);
+    }
+
+}
