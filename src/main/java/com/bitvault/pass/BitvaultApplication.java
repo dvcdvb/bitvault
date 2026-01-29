1748b74cf46f9bd325f654dd709288d45b500179
fab <fabiopalma2504@gmail.com>
2026-01-29 23:25:49 +0100

    feat: Added sample code to test h2 database

--- src/main/java/com/bitvault/pass/BitvaultApplication.java
+++ src/main/java/com/bitvault/pass/BitvaultApplication.java
@@ -2,12 +2,14 @@ package com.bitvault.pass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BitvaultApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BitvaultApplication.class, args);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
     }
-
 }
