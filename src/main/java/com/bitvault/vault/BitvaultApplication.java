262dc9fc9f7776f8ce9a1320962bb2d57a418c70
fab <fabiopalma2504@gmail.com>
2026-04-07 23:23:22 +0200

    fix: Edited main

--- src/main/java/com/bitvault/vault/BitvaultApplication.java
+++ src/main/java/com/bitvault/vault/BitvaultApplication.java
@@ -8,6 +8,6 @@ public class BitvaultApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BitvaultApplication.class, args);
-        System.out.println("Enter your name:");
+        System.out.println("Enter your nam");
     }
 }
